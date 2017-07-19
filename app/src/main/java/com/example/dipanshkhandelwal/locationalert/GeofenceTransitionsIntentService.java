0138a6e4c529ba7e36d8a153d40743d8f29173e8
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-07-20 03:15:08 +0530

    Added onHandleIntent

--- app/src/main/java/com/example/dipanshkhandelwal/locationalert/GeofenceTransitionsIntentService.java
+++ app/src/main/java/com/example/dipanshkhandelwal/locationalert/GeofenceTransitionsIntentService.java
@@ -1,8 +1,16 @@
 package com.example.dipanshkhandelwal.locationalert;
 
+import android.app.IntentService;
+import android.content.Intent;
+
 /**
  * Created by DIPANSH KHANDELWAL on 20-07-2017.
  */
 
-public class GeofenceTransitionsIntentService {
+public class GeofenceTransitionsIntentService extends IntentService{
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
 }
