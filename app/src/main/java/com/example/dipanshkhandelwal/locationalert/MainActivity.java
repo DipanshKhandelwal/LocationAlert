fc24d461914a40fe7a4bc476f61c80249c8550cb
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-07-22 02:21:57 +0530

    Added unimplemented methods

--- app/src/main/java/com/example/dipanshkhandelwal/locationalert/MainActivity.java
+++ app/src/main/java/com/example/dipanshkhandelwal/locationalert/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.dipanshkhandelwal.locationalert;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, ResultCallback<Status>{
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, ResultCallback<Status> {
 
     Button geofencesbutton ;
 
@@ -18,5 +25,25 @@ public class MainActivity extends AppCompatActivity GoogleApiClient.ConnectionCa
     }
 
     public void addGeofencesButtonHandler(View view){
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onResult(@NonNull Status status) {
+
     }
 }
